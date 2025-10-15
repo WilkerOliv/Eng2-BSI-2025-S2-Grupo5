f0c4e9849a74e2c692f6e592f2c739633abd3f91
Wilker <wilkeroliveira888.888@gmail.com>
2025-10-15 17:39:41 -0300

    Pastas organizadas

---
+++ Projeto/SALF_Backend/src/main/java/projeto/salf/SalfBackendApplication.java
@@ -0,0 +1,13 @@
+package projeto.salf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalfBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalfBackendApplication.class, args);
+    }
+
+}
---
+++ Projeto/SALF_Backend/src/test/java/projeto/salf/SalfBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package projeto.salf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalfBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
