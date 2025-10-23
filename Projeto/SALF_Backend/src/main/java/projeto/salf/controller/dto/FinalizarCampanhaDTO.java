a5b1d3e828f16ce637c36ceadbaa9db81fc892b2
DetzJao <gui_dezotti@hotmail.com>
2025-10-22 21:03:00 -0300

    Remoção DTO

--- Projeto/SALF_Backend/src/main/java/projeto/salf/controller/dto/FinalizarCampanhaDTO.java
+++
@@ -1,9 +0,0 @@
-package projeto.salf.controller.dto;
-
-import java.time.LocalDate;
-
-public class FinalizarCampanhaDTO {
-    public Double totalArrecadado;
-    public LocalDate dataFim; // opcional: se quiser atualizar a data de fim no ato da finalização
-    public String observacao; // opcional: observação de fechamento
-}
